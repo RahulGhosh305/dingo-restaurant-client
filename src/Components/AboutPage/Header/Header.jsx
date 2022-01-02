d982a069bd31517c585a27bcf760cb8f1191d3a3
Rahul Ghosh <rahulghosh096@gmail.com>
2022-01-03 01:58:21 +0600

    Redesign some component

--- src/Components/AboutPage/Header/Header.jsx
+++ src/Components/AboutPage/Header/Header.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from '../../Shared/Navbar/Navbar';
 import styles from './Header.module.css'
 const Header = () => {
     return (
-        <div className={styles.headerWrapper}>
-            <h2>About Us</h2>
-            <p>Information about our restaurant and our philosophy</p>
-            <div className="d-flex justify-content-center">
-                <nav aria-label="breadcrumb">
-                    <ol className="breadcrumb">
-                        <li className="breadcrumb-item"><Link to="/">Home</Link></li>
-                        <li className={`breadcrumb-item ${styles.colorWhite}`} aria-current="page">About Us</li>
-                    </ol>
-                </nav>
+        <>
+            <div className={styles.headerWrapper}>
+                <Navbar />
+                <div className={styles.headerTexts}>
+                    <h2>About Us</h2>
+                    <p>Information about our restaurant and our philosophy</p>
+                    <div className="d-flex justify-content-center">
+                        <nav aria-label="breadcrumb">
+                            <ol className="breadcrumb">
+                                <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+                                <li className={`breadcrumb-item ${styles.colorWhite}`} aria-current="page">About Us</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,51 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import Logo from '../../../utility/logo.png'
 
+
 //* NAVBAR COMPONENT
 const Navbar = () => {
+
+    //* Navbar Background Change
+    const [navbarColor, setNavbarColor] = useState(false)
+    const changeBackground = () => {
+        // console.log(window.scrollY);
+        if (window.scrollY >= 100) {
+            setNavbarColor(true)
+        }
+        else {
+            setNavbarColor(false)
+        }
+    }
+    window.addEventListener('scroll', changeBackground)
+
+
+    //* Navbar Collapsed with onClicked event on mobile device
+    const handleCollapse = () => {
+        const nav = document.getElementById("navbarSupportedContent");
+        const btn = document.getElementById("navbarBtn");
+        nav.classList.remove("show");
+        btn.classList.add("collapsed");
+    };
     return (
-        <header className={styles.navbarWrapper}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className={styles.navbarInner}>
-                            <nav className="navbar navbar-expand-lg navbar-light">
-                                <div className={`container-fluid ${styles.customContainer}`}>
-                                    <img src={Logo} alt="Icon" className="img-fluid" />
-                                    <button className={`navbar-toggler ${styles.navbarBtn}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                        <span className="navbar-toggler-icon"></span>
-                                    </button>
-                                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                        <ul className={`navbar-nav mx-auto mb-2 mb-lg-0 ${styles.navbarUl}`}>
-                                            <li className={`nav-item ${styles.navbarList}`}>
-                                                <Link className={`nav-link ${styles.listLink}`} to="/">Home</Link>
-                                            </li>
-                                            <li className={`nav-item ${styles.navbarList}`}>
-                                                <Link className={`nav-link ${styles.listLink}`} to="/about">About</Link>
-                                            </li>
-                                            <li className={`nav-item ${styles.navbarList}`}>
-                                                <Link className={`nav-link ${styles.listLink}`} to="/menus">Menus</Link>
-                                            </li>
-                                            <li className={`nav-item ${styles.navbarList}`}>
-                                                <Link className={`nav-link ${styles.listLink}`} to="/blogs">Blogs</Link>
-                                            </li>
-                                            <li className={`nav-item ${styles.navbarList}`}>
-                                                <Link className={`nav-link ${styles.listLink}`} to="/contact">Contact</Link>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
+        <>
+            <header className={styles.navbarWrapper}>
+                <div className={styles.navbarInner}>
+                    <nav className={navbarColor ? "navbar navbar-expand-lg fixed-top navbar-light bg-light" : "navbar navbar-expand-lg navbar-light"}>
+                        <div className={`container ${styles.customContainer}`}>
+                            <img src={Logo} alt="Icon" className="img-fluid" />
+                            <button className={`navbar-toggler ${styles.navbarBtn}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className={`navbar-nav mx-auto mb-2 mb-lg-0 ${styles.navbarUl}`}>
+                                    <li className={`nav-item ${styles.navbarList}`}>
+                                        <Link onClick={() => handleCollapse()} className={`nav-link ${styles.listLink}`} to="/">Home</Link>
+                                    </li>
+                                    <li className={`nav-item ${styles.navbarList}`}>
+                                        <Link onClick={() => handleCollapse()} className={`nav-link ${styles.listLink}`} to="/about">About</Link>
+                                    </li>
+                                    <li className={`nav-item ${styles.navbarList}`}>
+                                        <Link onClick={() => handleCollapse()} className={`nav-link ${styles.listLink}`} to="/menus">Menus</Link>
+                                    </li>
+                                    <li className={`nav-item ${styles.navbarList}`}>
+                                        <Link onClick={() => handleCollapse()} className={`nav-link ${styles.listLink}`} to="/blogs">Blogs</Link>
+                                    </li>
+                                    <li className={`nav-item ${styles.navbarList}`}>
+                                        <Link onClick={() => handleCollapse()} className={`nav-link ${styles.listLink}`} to="/contact">Contact</Link>
+                                    </li>
+                                </ul>
                                 <div className={styles.reservation}>
                                     <Link className={`nav-link ${styles.navLink}`} to="/">Reservation</Link>
                                 </div>
-                            </nav>
+                            </div>
                         </div>
-                    </div>
+                    </nav>
                 </div>
-            </div>
-        </header>
+            </header>
+        </>
     );
 };
 
